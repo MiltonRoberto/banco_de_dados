8fedd9a6db5a92fdc42f927d5e5f3570944d5d2c
Milton Roberto Bortolanza <127377404+MiltonRoberto@users.noreply.github.com>
2024-11-09 23:11:47 -0300

    Atualizar o ExercicioDW.sql

--- ExercicioDW.sql
+++ ExercicioDW.sql
@@ -1,3 +1,64 @@
+-- 1) Ciência da Computação, Data Warehouse, CESGRANRIO, 2018, Petrobras, Analista de
+-- Sistemas Júnior
+-- Ao construir um modelo de dados para um data warehouse de sua empresa, um
+-- desenvolvedor viu-se às voltas com três tabelas relacionais: venda, cliente e vendedor.
+-- Ao fazer uma transformação para o modelo estrela, ele deve organizar:
+-- A) venda, como tabela fato; cliente e vendedor, como tabelas dimensão 
+-- B) cliente e vendedor, como tabelas fato; venda, como tabela dimensão
+-- C) cliente, como tabela fato; venda e vendedor, como tabelas dimensão
+-- D) vendedor e venda, como tabelas fato; cliente, como tabela dimensão
+-- E) vendedor, como tabela fato; cliente e venda, como tabelas dimensão
+RESPOSTA - A
+
+-- 2) Ciência da Computação, Data Warehouse, CESPE / CEBRASPE, 2019, Secretaria da
+-- Fazenda do Estado - RS (SEFAZ/RS), Auditor Fiscal da Receita Estadual (Classe A)
+-- O data warehouse diferencia-se dos bancos de dados transacionais porque
+-- A) trabalha com dados atuais, mas não com dados históricos.
+-- B) faz uso intenso de operações diárias e de processamento de transações continuamente.
+-- C) possui milhares de usuários de diferentes níveis hierárquicos dentro da organização.
+-- D) tem dimensionalidade genérica e níveis de agregação ilimitados.
+-- E) utiliza ferramentas de prospecção e consulta de dados baseadas em OLTP (on-line
+-- transaction processing).
+RESPOSTA - D
+    
+-- 3) Ciência da Computação, Data Warehouse, FCC, 2018, Defensoria Pública do Estado de
+-- Amazonas - AM (DPE/AM), Analista em Gestão Especializado em TI
+-- Uma das características fundamentais de um ambiente de data warehouse está em
+-- A) servir como substituto aos bancos de dados operacionais de uma empresa, na
+-- eventualidade da ocorrência de problemas com tais bancos de dados.
+-- B) ser de utilização exclusiva da área de aplicações financeiras das empresas.
+-- C) proporcionar um ambiente que permita realizar análise dos negócios de uma empresa com base nos dados por ela armazenados.
+-- D) ser de uso prioritário de funcionários responsáveis pela área de telemarketing das
+-- empresas.
+-- E) armazenar apenas os dados mais atuais (máximo de 3 meses de criação),
+-- independentemente da área de atuação de cada empresa.
+RESPOSTA - C
+
+-- 4) Ciência da Computação, Data Warehouse, FCC, 2018, Defensoria Pública do Estado de
+-- Amazonas - AM (DPE/AM), Analista em Gestão Especializado em TI
+-- Sobre o processo de ETL, aplicado a data warehouse, é correto afirmar que
+-- A) a fase de extração de dados consiste em obter os dados do servidor do data warehouse.
+-- B) a fase de transformação consiste em realizar modificações nos dados carregados,
+-- adequando seus valores ao modelo definido para o data warehouse.
+-- C) as fases de extração e carga de dados são realizadas de forma simultânea.
+-- D) a fase de carga de dados visa eliminar valores nulos contidos nos bancos de dados
+-- transacionais da empresa.
+-- E) a fase de carga de dados consiste em inserir os dados transformados nos bancos de
+-- dados transacionais da empresa.
+RESPOSTA - B
+
+-- 5) Ciência da Computação, Data Warehouse, CESGRANRIO, 2018, Banco da Amazônia S/A
+-- - AM (BASA/AM), Técnico Científico
+-- Um Data Warehouse é recomendado para armazenar dados
+-- A) sumarizados de um departamento.
+-- B) sumarizados de toda a empresa para apoio à decisão e utilização de ferramentas OLAP.
+-- C) detalhados de toda a empresa para apoio à decisão e utilização de ferramentas OLAP.
+-- D) detalhados gerados por sistemas de informação transacionais.
+-- E) históricos detalhados de todas as transações realizadas em um determinado período de
+-- tempo.
+RESPOSTA - B
+
+
 -- Criação da tabela Dimensão Veículo
 CREATE TABLE Dimensao_Veiculo (
     Veiculo_ID INT PRIMARY KEY,
