d1173bbf07390f381f5ac622e9455c19694ef30a
Milton Roberto Bortolanza <127377404+MiltonRoberto@users.noreply.github.com>
2024-11-16 23:52:25 -0300

    Update 2.sql

--- Ex16.11/2.sql
+++ Ex16.11/2.sql
@@ -1,3 +1,4 @@
+
 CREATE TABLE Fato_Vendas (
     id_venda SERIAL PRIMARY KEY,
     id_cliente INT,
@@ -55,3 +56,38 @@ FROM Staging_Vendas;
 CREATE INDEX idx_cliente ON Fato_Vendas(id_cliente);
 CREATE INDEX idx_produto ON Fato_Vendas(id_produto);
 CREATE INDEX idx_tempo ON Fato_Vendas(id_tempo);
+
+INSERT INTO Dim_Cliente (nome, endereco, cidade, estado)
+SELECT nome_cliente, endereco_cliente, cidade_cliente, estado_cliente
+FROM Staging_Clientes;
+
+INSERT INTO Dim_Produto (nome_produto, categoria, preco)
+SELECT nome_produto, categoria_produto, preco_produto
+FROM Staging_Produtos;
+
+INSERT INTO Dim_Tempo (dia, mes, ano, trimestre, ano_mes)
+SELECT dia, mes, ano, trimestre, ano_mes
+FROM Staging_Tempo;
+
+INSERT INTO Fato_Vendas (id_cliente, id_produto, id_tempo, quantidade, valor_total)
+SELECT 
+    (SELECT id_cliente 
+     FROM Dim_Cliente 
+     WHERE nome = nome_cliente),
+    
+    (SELECT id_produto 
+     FROM Dim_Produto 
+     WHERE nome_produto = nome_produto),
+    
+    (SELECT id_tempo 
+     FROM Dim_Tempo 
+     WHERE ano_mes = ano_mes),
+    
+    quantidade, 
+    valor_total
+FROM Staging_Vendas;
+
+CREATE INDEX idx_cliente ON Fato_Vendas(id_cliente);
+CREATE INDEX idx_produto ON Fato_Vendas(id_produto);
+CREATE INDEX idx_tempo ON Fato_Vendas(id_tempo);
+
